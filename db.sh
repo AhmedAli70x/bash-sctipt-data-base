88fbd99103c762d0f7f3038e36df21ed90a17e3e
Ahmedm0det <ahmed.atef7x@gmail.com>
2020-10-29 22:40:20 +0200

    file1

---
+++ db.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PS3="MainMenu>"
+
+	printf "Your are logged into database engine\nYou can create database, Create table, Insert, Update or delete\n"
+	read use
+case $1 in
+
+esac
+
+create database
+create table
